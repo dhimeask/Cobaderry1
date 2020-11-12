8d1cfa3d13122ad1f176b99ad2ca7a8bf4c54b8b
STIGMA CLIENT <STIGMA CLIENT@DESKTOP-TKQECMO>
2020-11-12 18:39:38 +0700

    Project Coba

---
+++ src/cobaderry1/Cobaderry1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobaderry1;
+
+/**
+ *
+ * @author STIGMA CLIENT
+ */
+public class Cobaderry1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("cobaderry1.Cobaderry1.main()");
+    }
+   
+    
+}
